e4eeee02676a86793529e1e7b07dba34d4b33e25
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-09-11 10:09:07 +0200

    Add test

--- test/disk_usage_test.exs
+++ test/disk_usage_test.exs
@@ -2,7 +2,7 @@ defmodule DiskUsageTest do
   use ExUnit.Case
   doctest DiskUsage
 
-  test "greets the world" do
-    assert DiskUsage.hello() == :world
+  test "does not panic" do
+    assert DiskUsage.read() == :ok
   end
 end
