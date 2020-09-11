e6868a5967cc315b0ee4cc36d26b60baa38935a7
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-09-11 08:44:41 +0200

    $ mix new disk_usage

---
+++ lib/disk_usage.ex
@@ -0,0 +1,18 @@
+defmodule DiskUsage do
+  @moduledoc """
+  Documentation for `DiskUsage`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DiskUsage.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/disk_usage_test.exs
@@ -0,0 +1,8 @@
+defmodule DiskUsageTest do
+  use ExUnit.Case
+  doctest DiskUsage
+
+  test "greets the world" do
+    assert DiskUsage.hello() == :world
+  end
+end
